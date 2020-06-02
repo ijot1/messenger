0a34670ca200270b412d6e47d7b79194c441e720
ijot1 <ireneuszjaneczek128@gmail.com>
2020-06-03 00:23:39 +0200

    Initial commit

---
+++ src/test/java/com/ijot/messenger/messenger/MessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ijot.messenger.messenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessengerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
